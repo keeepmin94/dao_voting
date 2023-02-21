e79ae6d8ffab24da1bf0d1694ee0cf761a506cb3
jiUmin <wjdwkjh94@gmail.com>
2023-02-22 00:07:59 +0900

    fix readme

--- truffle-config.js
+++ truffle-config.js
@@ -89,7 +89,7 @@ module.exports = {
       timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     },
-    //https://goerli.infura.io/v3/6bdbc016315e4ba984f0305b7f6a82b4
+    
     // Useful for private networks
     // private: {
     //   provider: () => new HDWalletProvider(MNEMONIC, `https://network.io`),
